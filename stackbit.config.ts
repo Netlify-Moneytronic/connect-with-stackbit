1ca98f49cbeee36e8592ab69a46deec220a969e1
ChrisLaRocque <larocque.christopher@gmail.com>
2023-08-01 17:45:11 -0400

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,6 +3,7 @@ import { defineStackbitConfig } from "@stackbit/types";
 
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
+  ssgName: "nextjs",
   nodeVersion: "18",
   contentSources: [
     new ContentfulContentSource({
